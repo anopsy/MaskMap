4e8c1b9b2311fa3e7bb1c791e69c026e63642fe9
Magdalena <madkowalczuk@gmail.com>
2024-04-12 08:58:04 +0200

    basics

---
+++ README.md
@@ -0,0 +1,1 @@
+Using the power of NLP to gather data about most common masking behaviours in order to extend the database of masking behaviours in female ASD patients and to help patients and doctors identify them
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+import model  # This is your model inference module
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    prediction = model.predict(data)  # Call your model inference function
+    return {'prediction': prediction}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
